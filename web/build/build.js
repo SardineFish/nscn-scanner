04177790c980b660f0edad1f21fadef87cd735b0
SardineFish <Sardine@live.cn>
2021-04-20 15:00:22 +0800

    Create frontend project

---
+++ web/build/build.js
@@ -0,0 +1,29 @@
+const esbuild = require("esbuild");
+const fs = require("fs");
+// import { Plugin } from "esbuild";
+// import esbuild from "esbuild";
+
+const dev = process.argv.includes("--dev");
+const watch = process.argv.find(arg => arg === "-w" || arg === "--watch") !== undefined;
+const serveFiles = process.argv.find(arg => arg === "--serve" || arg === "-s") !== undefined;
+
+fs.copyFileSync("./src/views/index.html", "./dist/index.html");
+
+const serve = (config) => esbuild.serve({
+    servedir: "./dist",
+    host: "0.0.0.0",
+    port: 5000,
+}, config);
+
+const buildFunc = serveFiles ? serve : esbuild.build;
+
+buildFunc({
+    entryPoints: [
+        "src/views/index.tsx"
+    ],
+    outdir: "./dist/static/js",
+    minify: !dev,
+    watch: watch,
+    sourcemap: true,
+    bundle: true,
+});
---
+++ web/src/views/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.querySelector("#root");
+
+const App = (<>
+    HelloWorld
+</>);
+
+ReactDOM.render(App, rootElement);
