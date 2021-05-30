84be2c572d1ef6e1f3926a68b059d4efa6d56166
SardineFish <Sardine@live.cn>
2021-05-30 17:43:35 +0800

    Fix bugs cause stack-overflow

--- scanner_service/src/net_scanner/https_scanner.rs
+++ scanner_service/src/net_scanner/https_scanner.rs
@@ -13,10 +13,10 @@ use super::{result_handler::ScanResult, scanner::ScanTask};
 
 pub struct HttpsScanTask;
 impl HttpsScanTask {
-    async fn scan<S: AsyncRead + AsyncWrite + Unpin>(&self, stream: S) -> Result<HttpsResponse, SimpleError> {
+    async fn scan<S: AsyncRead + AsyncWrite + Unpin>(stream: S) -> Result<HttpsResponse, SimpleError> {
         // log::info!("Scan HTTPS {} through {}", self.addr, client.proxy_addr);
         timeout(Duration::from_secs(GLOBAL_CONFIG.scanner.https.timeout), async move {
-            let stream = self.connect_ssl(stream).await?;
+            let stream = Self::connect_tls(stream).await?;
 
             match stream.sync_ssl().peer_certificate() {
                 None => Err("No certificate")?,
@@ -29,7 +29,7 @@ impl HttpsScanTask {
             }
         }).await.map_err(|_|"Timeout")?
     }
-    async fn connect_ssl<S: AsyncRead + AsyncWrite + Unpin>(&self, stream: S) -> Result<async_ssl::SslStream<S>, SimpleError> {
+    async fn connect_tls<S: AsyncRead + AsyncWrite + Unpin>(stream: S) -> Result<async_ssl::SslStream<S>, SimpleError> {
         // log::info!("ESTABLISHED");
         let ssl = Ssl::new(&SSL_CONTEXT)?;
         let mut stream = async_ssl::SslStream::new(ssl, stream)?;
@@ -47,7 +47,7 @@ impl ScanTask<HttpsResponse> for HttpsScanTask {
         "https"
     }
     async fn scan<S: Send + Sync + AsyncRead + AsyncWrite + Unpin + 'static>(self, stream: &mut S) -> Result<HttpsResponse, SimpleError> {
-        self.scan(stream).await
+        Self::scan(stream).await
     }
 }
 
