0c0327f7f5c62167cd190b05849404152914a32b
SardineFish <Sardine@live.cn>
2021-06-08 00:16:39 +0800

    Add ss proxy test

--- scanner_service/src/proxy/ss_proxy.rs
+++ scanner_service/src/proxy/ss_proxy.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use shadowsocks::{self, ProxyClientStream, ServerConfig, context::Context};
 
-use crate::{SSProxyConfig, error::SimpleError, net_scanner::scanner::Connector};
+use crate::{error::SimpleError, net_scanner::scanner::Connector};
 
 pub type SSProxyStream = ProxyClientStream<shadowsocks::net::TcpStream>;
 
@@ -18,4 +18,30 @@ impl Connector<SSProxyStream> for SSProxy {
         let stream = ProxyClientStream::connect(self.ctx, &self.cfg, (addr.to_owned(), port)).await?;
         Ok(stream)
     }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::config::*;
+    use crate::proxy::ss_proxy::SSProxy;
+    use crate::net_scanner::scanner::Connector;
+    use crate::net_scanner::http_scanner::HttpScanTask;
+    use crate::net_scanner::scanner::ScanTask;
+
+    #[tokio::test]
+    async fn test_ss_proxy() {
+        if let Some(cfg_list) = &GLOBAL_CONFIG.proxy_pool.shadowsocks {
+            for cfg in cfg_list {
+                let proxy = SSProxy {
+                    cfg,
+                    ctx: shadowsocks::context::Context::new_shared(shadowsocks::config::ServerType::Local),
+                };
+                
+                let mut stream = proxy.connect("myip.top", 80).await.unwrap();
+                let result = HttpScanTask("myip.top".to_owned(), 80).scan(&mut stream).await.unwrap();
+                assert_eq!(result.status, 200);
+            }
+        }
+    }
+    
 }
