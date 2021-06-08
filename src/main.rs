df837a3d549edd8903d23d91de3d27b140e58048
SardineFish <Sardine@live.cn>
2021-06-09 00:06:17 +0800

    Remove unused code

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,6 @@ use model::Model;
 use std::{ops::Range, str::FromStr};
 
 use actix_web::{App, web::{self, JsonConfig}};
-use futures::stream::StreamExt;
-use mongodb::bson::{doc, Document};
-use mongodb::Database;
 use nscn::{GLOBAL_CONFIG, MasterService, error::*};
 use nscn::{self, WorkerService};
 use tokio::{
@@ -73,8 +70,8 @@ async fn main() {
     let join = task::spawn(server);
 
     if let Some(master) = master {
-        task::spawn(try_dispatch_address(master.clone()));
-        task::spawn(try_dispatch_analysing(db.clone(), master.clone()));
+        // task::spawn(try_dispatch_address(master.clone()));
+        // task::spawn(try_dispatch_analysing(db.clone(), master.clone()));
         task::spawn(connect_workers(master));
     }
     task::spawn(connect_master(worker.clone()));
@@ -119,65 +116,65 @@ async fn connect_workers(master: MasterService) {
     }
 }
 
-async fn try_dispatch_address(service: MasterService) {
-    let dispatcher = service.scanner().scheduler().dispathcer();
-    if !service.config().scanner.task.fetch {
-        return;
-    }
-    log::info!("Start dispatching http scan address");
-    if service.config().scanner.task.clear_old_tasks {
-        if let Err(err) = dispatcher.clear_tasks().await {
-            log::error!("Failed to reset task queue: {}", err.msg);
-        }
-    }
-    for url in &service.config().scanner.task.addr_src {
-        let list = loop {
-            match service.fetch_address_list(&url).await {
-                Err(err) => log::error!("Failed to fetch address list from '{}': {}", url, err.msg),
-                Ok(list) => break list,
-            };
-            sleep(Duration::from_secs(1)).await;
-        };
+// async fn try_dispatch_address(service: MasterService) {
+//     let dispatcher = service.scanner().scheduler().dispathcer();
+//     if !service.config().scanner.task.fetch {
+//         return;
+//     }
+//     log::info!("Start dispatching http scan address");
+//     if service.config().scanner.task.clear_old_tasks {
+//         if let Err(err) = dispatcher.clear_tasks().await {
+//             log::error!("Failed to reset task queue: {}", err.msg);
+//         }
+//     }
+//     for url in &service.config().scanner.task.addr_src {
+//         let list = loop {
+//             match service.fetch_address_list(&url).await {
+//                 Err(err) => log::error!("Failed to fetch address list from '{}': {}", url, err.msg),
+//                 Ok(list) => break list,
+//             };
+//             sleep(Duration::from_secs(1)).await;
+//         };
 
-        let mut count = 0;
-        log::info!("Get {} address range from {}", list.len(), url);
-        for ip_cidr in list {
-            let range = match parse_ipv4_cidr(&ip_cidr) {
-                Err(err) => {
-                    log::error!("{}", err.msg);
-                    continue;
-                }
-                Ok(range) => range,
-            };
-            count += range.len();
-            if let Err(err) = dispatcher.enqueue_task(&ip_cidr).await {
-                log::error!("Failed to enqueue http scan task: {}", err.msg);
-            }
-        }
-        log::info!("Enqueue {} address", count);
-    }
-}
+//         let mut count = 0;
+//         log::info!("Get {} address range from {}", list.len(), url);
+//         for ip_cidr in list {
+//             let range = match parse_ipv4_cidr(&ip_cidr) {
+//                 Err(err) => {
+//                     log::error!("{}", err.msg);
+//                     continue;
+//                 }
+//                 Ok(range) => range,
+//             };
+//             count += range.len();
+//             if let Err(err) = dispatcher.enqueue_task(&ip_cidr).await {
+//                 log::error!("Failed to enqueue http scan task: {}", err.msg);
+//             }
+//         }
+//         log::info!("Enqueue {} address", count);
+//     }
+// }
 
-async fn try_dispatch_analysing(db: Database, scanner: MasterService) {
-    let dispatcher = scanner.analyser().dispathcer();
-    let query = doc! {
-        "addr": "0.0.0.0",
-        "$or": [
-            {"scan.http.success": { "$gt": 0}},
-            {"scan.tcp.21.ftp.success": { "$gt": 0}},
-            {"scan.tcp.22.ssh.success": { "$gt": 0}},
-        ],
-    };
-    let mut cursor = db
-        .collection::<Document>("scan")
-        .find(query, None)
-        .await
-        .unwrap();
-    while let Some(Ok(doc)) = cursor.next().await {
-        let addr = doc.get_str("addr").unwrap();
-        dispatcher.enqueue_task(addr).await.unwrap();
-    }
-}
+// async fn try_dispatch_analysing(db: Database, scanner: MasterService) {
+//     let dispatcher = scanner.analyser().dispathcer();
+//     let query = doc! {
+//         "addr": "0.0.0.0",
+//         "$or": [
+//             {"scan.http.success": { "$gt": 0}},
+//             {"scan.tcp.21.ftp.success": { "$gt": 0}},
+//             {"scan.tcp.22.ssh.success": { "$gt": 0}},
+//         ],
+//     };
+//     let mut cursor = db
+//         .collection::<Document>("scan")
+//         .find(query, None)
+//         .await
+//         .unwrap();
+//     while let Some(Ok(doc)) = cursor.next().await {
+//         let addr = doc.get_str("addr").unwrap();
+//         dispatcher.enqueue_task(addr).await.unwrap();
+//     }
+// }
 
 pub fn parse_ipv4_cidr(cidr: &str) -> Result<Range<u32>, SimpleError> {
     let slices: Vec<&str> = cidr.split("/").collect();
